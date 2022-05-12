d062399bef72b57f0c1651abc9558f1a1c5e7fc7
noobnewbier <peter789b@gmail.com>
2022-05-13 01:41:23 +0900

    chore: coffee maker components now requires specific modules

--- Noneb.Jet.Playground/Coffee/Di/IEspressoMakerComponent.cs
+++ Noneb.Jet.Playground/Coffee/Di/IEspressoMakerComponent.cs
@@ -1,7 +1,10 @@
-﻿using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+﻿using Noneb.Jet.Core;
+using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+using Noneb.Jet.Playground.Coffee.ManualDi;
 
 namespace Noneb.Jet.Playground.Coffee.Di;
 
+[Requires(typeof(EspressoModule))]
 public interface IEspressoMakerComponent
 {
     CoffeeMaker CoffeeMaker { get; }
--- Noneb.Jet.Playground/Coffee/Di/ILatteMakerComponent.cs
+++ Noneb.Jet.Playground/Coffee/Di/ILatteMakerComponent.cs
@@ -1,7 +1,10 @@
-﻿using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+﻿using Noneb.Jet.Core;
+using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+using Noneb.Jet.Playground.Coffee.ManualDi;
 
 namespace Noneb.Jet.Playground.Coffee.Di;
 
+[Requires(typeof(LatteModule))]
 public interface ILatteMakerComponent
 {
     CoffeeMaker CoffeeMaker { get; }
