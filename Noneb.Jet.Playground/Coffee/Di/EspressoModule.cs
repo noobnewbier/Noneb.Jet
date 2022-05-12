fbb2c1d64d5df3f76a639f76890ac1dca9a6f7ac
noobnewbier <peter789b@gmail.com>
2022-05-13 01:47:39 +0900

    chore: marking respective properties in coffee modules with `Provides`

--- Noneb.Jet.Playground/Coffee/Di/EspressoModule.cs
+++ Noneb.Jet.Playground/Coffee/Di/EspressoModule.cs
@@ -1,9 +1,13 @@
-﻿using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+﻿using Noneb.Jet.Core;
+using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
 
 namespace Noneb.Jet.Playground.Coffee.Di;
 
 public class EspressoModule
 {
+    [Provides]
     public IBean Bean => new EspressoBean();
+    
+    [Provides]
     public Heater Heater => new();
 }
--- Noneb.Jet.Playground/Coffee/Di/LatteModule.cs
+++ Noneb.Jet.Playground/Coffee/Di/LatteModule.cs
@@ -1,9 +1,13 @@
-﻿using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
+﻿using Noneb.Jet.Core;
+using Noneb.Jet.Playground.Coffee.Core.CoffeeMakers;
 
 namespace Noneb.Jet.Playground.Coffee.Di;
 
 public class LatteModule
 {
+    [Provides]
     public IBean Bean => new LatteBean();
+    
+    [Provides]
     public IHeater Heater => new Heater();
 }
