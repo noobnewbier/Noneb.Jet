0435a6c1f5cf6801c7ed3a58d4fd56dd7a67e9d4
noobnewbier <peter789b@gmail.com>
2022-06-01 08:44:32 +0900

    refactor(component): create components template

---
+++ Noneb.Jet.Analyzer/CodeTemplates/ComponentFactoryTemplate.cs
@@ -0,0 +1,23 @@
+﻿namespace Noneb.Jet.Analyzer.CodeTemplates;
+
+public static class ComponentFactoryTemplate
+{
+    public static string CreateComponentFactory(IEnumerable<string> dependencyNamespaces, string createdInterfaceName)
+    {
+        var generatedFactory =
+@$"{string.Join("\n",dependencyNamespaces.Select(n => $"using {n};"))};
+
+namespace Noneb.Jet.Core
+{{
+    public static partial class ComponentFactory
+    {{
+        public static {createdInterfaceName} Create{createdInterfaceName}()
+        {{
+            return new GeneratedImplOf{createdInterfaceName}();
+        }}
+    }}
+}}";
+
+        return generatedFactory;
+    }
+}
---
+++ Noneb.Jet.Analyzer/CodeTemplates/ComponentTemplate.cs
@@ -0,0 +1,17 @@
+﻿namespace Noneb.Jet.Analyzer.CodeTemplates;
+
+public static class ComponentTemplate
+{
+    public static string CreateComponent(string createdInterfaceNamespace, string createdInterfaceName)
+    {
+        var generatedComponent = 
+@$"namespace {createdInterfaceNamespace}
+{{
+    public class GeneratedImplOf{createdInterfaceName} : {createdInterfaceName}
+    {{
+    }}
+}}";
+
+        return generatedComponent;
+    }
+}
